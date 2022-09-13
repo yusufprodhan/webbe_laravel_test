1a369a0283b95a0be38d778953f85f008b2cb9f1
Md Yusuf Prodhan <yusuf@supremesupports.com.au>
2022-09-13 09:04:15 +0600

    get Events With Workshops  done

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -4,7 +4,12 @@ namespace App\Models;
 
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Event extends Model
 {
+    public function workshop(): HasOne
+    {
+        return $this->hasOne(Workshop::class);
+    }
 }
