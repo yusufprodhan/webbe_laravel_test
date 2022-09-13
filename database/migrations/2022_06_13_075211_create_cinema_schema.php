0d67043f2605d95257f664ba52a390877cb7b6da
Md Yusuf Prodhan <yusuf@supremesupports.com.au>
2022-09-13 08:14:31 +0600

    cinema_schema migration updated

--- database/migrations/2022_06_13_075211_create_cinema_schema.php
+++ database/migrations/2022_06_13_075211_create_cinema_schema.php
@@ -37,7 +37,44 @@ class CreateCinemaSchema extends Migration
      */
     public function up()
     {
-        throw new \Exception('implement in coding task 4, you can ignore this exception if you are just running the initial migrations.');
+        Schema::create('cinema', function (Blueprint $table) {
+            $table->id();
+            $table->uuid('uuid');
+            $table->integer('user_id')->unsigned();
+            $table->integer('event_id')->unsigned();
+            $table->integer('booked')->default(0); //0 for can  book and 1 for book out
+            $table->string('name')->unique();
+            $table->double("price",10,2);
+            $table->integer("total_capacity");
+            $table->double("offer_price",10,2)->nullable();
+            $table->integer("discount")->nullable();
+            $table->date('release')->nullable();
+            $table->year("release_year");
+            $table->string('location')->nullable();
+            $table->string('summary')->nullable();
+            $table->string('logo')->nullable();
+            $table->foreign('event_id')->references('id')->on('events')->onDelete('cascade');
+            $table->timestamps();
+        });
+
+        Schema::create('booking', function (Blueprint $table) {
+            $table->id();
+            $table->uuid('uuid');
+            $table->integer('seat_id')->unsigned();
+            $table->dateTime('reserved_slot');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('seat_id')->references('id')->on('seat')->onDelete('cascade');
+            $table->timestamps();
+        });
+        Schema::create('seat', function (Blueprint $table) {
+            $table->id();
+            $table->enum('seat_type',['VIP','COUPLE','SUPER_VIP','GENERAL','OTHERS']);
+            $table->integer('count');
+            $table->string('position');
+            $table->integer('cinema_id')->unsigned();
+            $table->foreign('cinema_id')->references('id')->on('cinema')->onDelete('cascade');
+            $table->timestamps();
+        });
     }
 
     /**
@@ -47,5 +84,7 @@ class CreateCinemaSchema extends Migration
      */
     public function down()
     {
+        Schema::dropIfExists('cinema');
+        Schema::dropIfExists('booking');
     }
 }
